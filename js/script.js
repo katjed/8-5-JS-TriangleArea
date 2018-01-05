27556996d3fc2be293258f5cfc7bbb07aef7ea4d
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-01-05 07:22:11 +0100

    Change index.html, change js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Method 1
+
+var a = 3,
+	h = 4,
+	triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+// Method 2
+
+var a = prompt('Enter base a: '),
+	h = prompt('Enter height h: '),
+	triangleArea = a*h/2;
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
