4618ba249687c4229082ce341e492b79979ecd25
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-01-06 02:37:50 +0100

    Change .gitignore, change js, change package-lock.json

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-// Method 1
+// method 1
 
 var a = 3,
 	h = 4,
 	triangleArea = a*h/2;
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
 
-// Method 2
+// method 2
 
 var a = prompt('Enter base a: '),
 	h = prompt('Enter height h: '),
