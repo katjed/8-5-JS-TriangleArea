db48a574860c07669d1fc884c8479e64c43c19ae
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-02-04 21:07:58 +0100

    Update files

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
-// method 1
+// Method 1
 
 var a = 3,
 	h = 4,
 	triangleArea = a * h / 2;
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
 
-// method 2
-
+// Method 2
 var a = prompt('Enter base a: '),
 	h = prompt('Enter height h: '),
 	triangleArea = a * h / 2;
